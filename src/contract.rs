e4224d0a4a69244b82deb2db8fa28d99b6c3b4ac
boney <boney.fernandez@metaschool.so>
2024-07-26 14:47:13 +0530

    cleanup

--- src/contract.rs
+++ src/contract.rs
@@ -3,21 +3,10 @@ use cosmwasm_std::{
     StdError,
 };
 use cw2::set_contract_version;
-use cw_storage_plus::Item;
+use crate::state::{State, STATE};
+use crate::msg::InstantiateMsg;
 use serde::{Deserialize, Serialize};
 use schemars::JsonSchema;
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub message: String,
-}
-
-pub const STATE: Item<State> = Item::new("state");
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateMsg {
-    pub message: String,
-}
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -32,11 +21,9 @@ pub fn instantiate(
     Ok(Response::default())
 }
 
-#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(_deps: Deps, _env: Env, _msg: Binary) -> StdResult<Binary> {
     match STATE.load(_deps.storage) {
         Ok(state) => to_json_binary(&state.message),
         Err(_) => Err(StdError::not_found("state")),
     }
 }
-
